c777484513dc021352922da88a17cda7ad3d52f9
Mikhail Subach <mikhail_subach@epam.com>
2021-04-26 20:54:36 +0300

    tests added

--- tests/converter.test.js
+++ tests/converter.test.js
@@ -4,4 +4,11 @@ describe('converter', () => {
   test('format date to YYYY-MM-DD format', () => {
     expect(converter.formatDate(new Date("2021-03-31"))).toBe("2021-03-31");
   })
+  test('formatDateToUSFromDays', () => {
+    expect(converter.formatDateToUSFromDays(2)).toBe("1/2/1900");
+  })
+  test('formatDateFromDays', () => {
+    // if UTC+3, it would be -1 day
+    expect(converter.formatDateFromDays(3)).toBe("1900-01-02");
+  })
 })
